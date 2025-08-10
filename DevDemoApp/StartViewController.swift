d8a3bc74600b467e73ec873c3b0109306e4a439c
Ronald Jones <rondjonesjr@gmail.com>
2025-08-10 18:57:12 -0400

    first commit

---
+++ DevDemoApp/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  DevDemoApp
+//
+//  Created by Ronald Jones on 8/10/25.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
