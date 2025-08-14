ecb8c9a40a9df2ea96d331ee104ee1569fa849ee
Ronald Jones <rondjonesjr@gmail.com>
2025-08-14 15:43:42 -0400

    launch demo

--- DevDemoApp/StartViewController.swift
+++ DevDemoApp/StartViewController.swift
@@ -11,10 +11,8 @@ class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+     
         // Do any additional setup after loading the view.
     }
-    
 
 }
