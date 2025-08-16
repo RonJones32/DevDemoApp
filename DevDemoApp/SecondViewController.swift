21c23b2abfeace11c5c0837b94e77efbb7c19dae
alkanb <buket.alkan@vanderbilt.edu>
2025-08-16 15:48:13 -0500

    navigation to next page is added

---
+++ DevDemoApp/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  DevDemoApp
+//
+//  Created by Buket on 8/16/25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DevDemoApp/StartViewController.swift
+++ DevDemoApp/StartViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
      
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapNext(_ sender: UIButton) {
+        // debugging statement to see if the connection is set
+        print("didTapNext fired")
+        
+        let nextVC = SecondViewController()
+        
+        // pushes it onto the navigation stack
+        navigationController?.pushViewController(nextVC, animated: true)
+
+        
     }
 
 }
