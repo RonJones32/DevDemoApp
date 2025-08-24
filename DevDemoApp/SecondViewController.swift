88e42472d080f78e7f923e1c8e471a70fcc552cf
ZachK17 <zacharyrk17@gmail.com>
2025-08-24 17:08:59 -0400

    August 24th, 2025

--- DevDemoApp/SecondViewController.swift
+++ DevDemoApp/SecondViewController.swift
@@ -8,20 +8,24 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    //SecondPicLabel creates the UILabel as a variable
     @IBOutlet weak var SecondPicLabel : UILabel!
+    //SecondImageView creates the UIImageView as a variable
     @IBOutlet weak var SecondImageView : UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Text to describe picture
         SecondPicLabel.text = "This is Evil Mochi! Watch Out!"
-        
+        //Shows an image from an asset called Evil Mochi
         SecondImageView.image = UIImage(named: "Evil Mochi")
+        //Scales image to fit within set boundaries
         SecondImageView.contentMode = .scaleAspectFit
 
         // Do any additional setup after loading the view.
     }
-
+    
+    //Function creates a dismissButton that will dismiss the page 
     @IBAction func dismissButton(_ sender : UIButton) {
         dismiss(animated: true, completion: nil)
     }
--- DevDemoApp/StartViewController.swift
+++ DevDemoApp/StartViewController.swift
@@ -9,16 +9,19 @@ import UIKit
 
 class StartViewController: UIViewController {
 
-
+    //picLabel creates the UILabel as a variable
     @IBOutlet weak var picLabel : UILabel!
+    //imageView creates the UIImageView as a variable
     @IBOutlet weak var imageView : UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Text to describe image
         picLabel.text = "This is Mochi!"
-        
+        //Shows image from an asset called "Mochi"
         imageView.image = UIImage(named: "Mochi")
+        //Scales image to fit within set boundaries
         imageView.contentMode = .scaleAspectFit
      
         // Do any additional setup after loading the view.
@@ -30,10 +33,10 @@ class StartViewController: UIViewController {
         
        let nextVC = SecondViewController()
         
-        // pushes it onto the navigation stack
+        //This is a controller that opens a new window that can be dismissed
         //navigationController?.pushViewController(nextVC, animated: true)
+        
         //This is the updated controller opening a new window that doens't dismiss
-        //This was done by Zach Keyser
         nextVC.modalPresentationStyle = .fullScreen
         self.present(nextVC, animated: true)
        
